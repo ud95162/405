4af28854856bfb0648360240d643ca2293ffe9d0
ud95162 <42994429+ud95162@users.noreply.github.com>
2019-07-27 14:24:32 +0530

    add

--- Team405.java
+++ Team405.java
@@ -13,10 +13,11 @@ import robocode.HitRobotEvent;
 import robocode.ScannedRobotEvent;
 
 import java.awt.*;
-import robocode.AlphaBot;
+
 
 import robocode.WinEvent;
 import static robocode.util.Utils.normalRelativeAngleDegrees;
+import robocode.BravoBot;
 
 
 
@@ -28,8 +29,9 @@ import static robocode.util.Utils.normalRelativeAngleDegrees;
  * @author Mathew A. Nelson (original)
  * @author Flemming N. Larsen (contributor)
  */
-public class Team405 extends AlphaBot {
+public class Team405 extends BravoBot{
 
+    int turnDirection = 1;
 	boolean peek; // Don't turn if there's a robot there
 	double moveAmount; // How much to move
         int others; // Number of other robots in the game
@@ -51,47 +53,46 @@ public class Team405 extends AlphaBot {
 		setRadarColor(Color.orange);
 		setBulletColor(Color.cyan);
 		setScanColor(Color.cyan);
-
-                
-                
-                
+                                              
                 
 		// Initialize moveAmount to the maximum possible for this battlefield.
 		moveAmount = Math.max(getBattleFieldWidth(), getBattleFieldHeight());
 		// Initialize peek to false
 		peek = false;
+                                        
+                turnLeft(getHeading() % 90);
+		ahead(moveAmount);
+		// Turn the gun to turn right 90 degrees.
+		peek = true;
+		turnGunRight(90);
+		turnRight(90);
 
-                
-                trackName = null; // Initialize to not tracking anyone
-		setAdjustGunForRobotTurn(true); // Keep the gun still when we turn
-		gunTurnAmt = 10; // Initialize gunTurn to 10
-
-		// Loop forever
 		while (true) {
-			// turn the Gun (looks for enemy)
-			turnGunRight(gunTurnAmt);
-			// Keep track of how long we've been looking
-			count++;
-			// If we've haven't seen our target for 2 turns, look left
-			if (count > 2) {
-				gunTurnAmt = -10;
-			}
-			// If we still haven't seen our target for 5 turns, look right
-			if (count > 5) {
-				gunTurnAmt = 10;
-			}
-			// If we *still* haven't seen our target after 10 turns, find another target
-			if (count > 11) {
-				trackName = null;
-			}
+			// Look before we turn when ahead() completes.
+			peek = true;
+			// Move up the wall
+			ahead(100);
+			// Don't look now
+			peek = false;
+			// Turn to the next wall
+			turnRight(90);
 		}
-		// turnLeft to face a wall.
-		// getHeading() % 90 means the remainder of
-		// getHeading() divided by 90.
-//		turnLeft(getHeading() % 90);
-//		ahead(moveAmount);
-//		// Turn the gun to turn right 90 degrees.
-//		peek = true;
+                
+//                
+                
+                
+                
+                
+                
+                
+                
+		 //turnLeft to face a wall.
+		 //() % 90 means the remainder of
+		 //getHeading() divided by 90.
+		//turnLeft(getHeading() % 90);
+		//ahead(moveAmount);
+		// Turn the gun to turn right 90 degrees.
+		//peek = true;
 //		turnGunRight(90);
 //		turnRight(90);
 //                   
@@ -106,7 +107,7 @@ public class Team405 extends AlphaBot {
 //			turnRight(90);
 //		}
 //                
-//               
+               
 	}
 
 	/**
@@ -114,6 +115,31 @@ public class Team405 extends AlphaBot {
 	 */
 	public void onHitRobot(HitRobotEvent e) {
 		// If he's in front of us, set back up a bit.
+            
+            
+            if (e.getBearing() >= 0) {
+			turnDirection = 1;
+		} else {
+			turnDirection = -1;
+		}
+		turnRight(e.getBearing());
+
+		// Determine a shot that won't kill the robot...
+		// We want to ram him instead for bonus points
+		if (e.getEnergy() > 16) {
+			fire(3);
+		} else if (e.getEnergy() > 10) {
+			fire(2);
+		} else if (e.getEnergy() > 4) {
+			fire(1);
+		} else if (e.getEnergy() > 2) {
+			fire(.5);
+		} else if (e.getEnergy() > .4) {
+			fire(.1);
+		}
+		ahead(40);
+            
+            
 		if (e.getBearing() > -90 && e.getBearing() < 90) {
 			back(100);
                         fire(3);
@@ -140,7 +166,7 @@ public class Team405 extends AlphaBot {
 		// An AdvancedRobot might use setBack(); execute();
 		gunTurnAmt = normalRelativeAngleDegrees(e.getBearing() + (getHeading() - getRadarHeading()));
 		turnGunRight(gunTurnAmt);
-		fire(30);
+		fire(4);
 		back(50);
 	}
 
@@ -182,6 +208,9 @@ public class Team405 extends AlphaBot {
 		//if (peek) {
 		//	scan();
 		//}
+                 
+                
+                //trackfire
                 double absoluteBearing = getHeading() + e.getBearing();
 		double bearingFromGun = normalRelativeAngleDegrees(absoluteBearing - getGunHeading());
 
@@ -199,6 +228,44 @@ public class Team405 extends AlphaBot {
 		else {
 			turnGunRight(bearingFromGun);
 		}
+		// Generates another scan event if we see a robot.
+		// We only need to call this if the gun (and therefore radar)
+		// are not turning.  Otherwise, scan is called automatically.
+		if (bearingFromGun == 0) {
+			scan();
+		}
+                
+                
+                
+              //tracker  
+                
+//                if (peek) {
+//			scan();
+//		}
+//                
+//                double absoluteBearing = getHeading() + e.getBearing();
+//		double bearingFromGun = normalRelativeAngleDegrees(absoluteBearing - getGunHeading());
+//
+//		// If it's close enough, fire!
+//		if (Math.abs(bearingFromGun) <= 3) {
+//			turnGunRight(bearingFromGun);
+//			// We check gun heat here, because calling fire()
+//			// uses a turn, which could cause us to lose track
+//			// of the other robot.
+//			if (getGunHeat() == 0) {
+//				fire(Math.min(3 - Math.abs(bearingFromGun), getEnergy() - .1));
+//			}
+//		} // otherwise just set the gun to turn.
+//		// Note:  This will have no effect until we call scan()
+//		else {
+//			turnGunRight(bearingFromGun);
+//		}
+                
+                
+                
+                
+                
+                
 		// Generates another scan event if we see a robot.
 		// We only need to call this if the gun (and therefore radar)
 		// are not turning.  Otherwise, scan is called automatically.
@@ -250,7 +317,7 @@ public class Team405 extends AlphaBot {
 		// Our target is close.
 		gunTurnAmt = normalRelativeAngleDegrees(e.getBearing() + (getHeading() - getRadarHeading()));
 		turnGunRight(gunTurnAmt);
-		fire(3);
+		fire(2);
 
 		// Our target is too close!  Back up.
 		if (e.getDistance() < 100) {
@@ -264,9 +331,9 @@ public class Team405 extends AlphaBot {
 	}
 
 	public void onWin(WinEvent e) {
-		for (int i = 0; i < 50; i++) {
-			turnRight(30);
-			turnLeft(30);
+		for (int i = 0; i < 10000; i++) {
+			turnRight(100);
+			turnLeft(100);
 		}
 	}
         
